7786d028354fb4d02dd84ef630cd81b5f2387b70
hmartiins <hmartins224@gmail.com>
2021-01-14 19:32:51 -0300

    performing route authentication

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+
+import Route from './Routes';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+
+import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/singup" exact component={SignUp} />
+    <Route path="/dashboard" exact component={Dashboard} isPrivate />
   </Switch>
 );
 
